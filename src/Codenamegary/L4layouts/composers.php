4df693a9f6b0b4068375a81f00dc4d454218e46f
Gary Saunders <gary@powrit.com>
2013-04-27 21:02:49 -0400

    Resolving asset collections from the IoC container, minor refactor for cleanliness.

--- src/Codenamegary/L4layouts/composers.php
+++ src/Codenamegary/L4layouts/composers.php
@@ -1,56 +1,38 @@
 <?php
 
 $secure = Config::get('l4layouts::settings.ssl',false);
-    
-View::composer(array('l4layouts::bootstrap.blank'),function($view)use($secure)
+$assets = App::make('l4sacs');    
+$baseMeta = array(
+    'description'   => '',
+    'keywords'      => '',
+    'author'        => '',
+    'charset'       => 'UTF-8',
+);
+
+View::composer(array('l4layouts::bootstrap.blank'),function($view)use($secure,$assets,$baseMeta)
 {
-
-
-
-    // Grab the existing data bound to the view
-    $existingData = $view->getData();
-    
-    // If assets have already been injected into the view, use them. 
-    $assets = isset( $existingData[ 'assets' ] ) ? $existingData[ 'assets' ] : new Codenamegary\L4layouts\AssetCollection( 'assets' ) ;
     $assets->collection('header')->addCss( asset('packages/codenamegary/l4layouts/bootstrap/css/bootstrap.min.css', $secure ) );
     $assets->collection('footer')->addJs( asset('packages/codenamegary/l4layouts/bootstrap/js/bootstrap.min.js', $secure ) );
     $view->with( 'assets', $assets );
 
-    $baseMeta = array(
-        'description'   => '',
-        'keywords'      => '',
-        'author'        => '',
-        'charset'       => 'UTF-8',
-    );
-
+    // Grab the existing data bound to the view
+    $existingData = $view->getData();
     $meta = isset( $existingData[ 'meta' ] ) ? array_merge( $baseMeta, $existingData[ 'meta' ] ) : $baseMeta ;
     $view->with( 'meta', $meta );
     
 });
 
-View::composer(array('l4layouts::foundation.blank'),function($view)use($secure)
+View::composer(array('l4layouts::foundation.blank'),function($view)use($secure,$assets,$baseMeta)
 {
-
-    // Grab the existing data bound to the view
-    $existingData = $view->getData();
-    
-    // If assets have already been injected into the view, use them. 
-    $assets = isset( $existingData[ 'assets' ] ) ? $existingData[ 'assets' ] : new Codenamegary\L4layouts\AssetCollection( 'assets' ) ;
     // Init and create a footer collection if one doesn't already exist
     $assets->collection('footer');
     $assets->collection('header')->addCss( asset('packages/codenamegary/l4layouts/foundation/css/normalize.css', $secure ) );
     $assets->collection('header')->addCss( asset('packages/codenamegary/l4layouts/foundation/css/foundation.min.css', $secure ) );
     $assets->collection('header')->addJs( asset('packages/codenamegary/l4layouts/foundation/js/foundation.min.js', $secure ) );
-
     $view->with( 'assets', $assets );
-    
-    $baseMeta = array(
-        'description'   => '',
-        'keywords'      => '',
-        'author'        => '',
-        'charset'       => 'UTF-8',
-    );
 
+    // Grab the existing data bound to the view
+    $existingData = $view->getData();
     $meta = isset( $existingData[ 'meta' ] ) ? array_merge( $baseMeta, $existingData[ 'meta' ] ) : $baseMeta ;
     $view->with( 'meta', $meta );
     
