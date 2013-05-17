934a8d2880a5f639bb5f810857f6a4e76429acfe
Gary Saunders <gary@powrit.com>
2013-05-16 23:22:24 -0400

    Have to check array member exists first.

--- src/Codenamegary/L4layouts/composers.php
+++ src/Codenamegary/L4layouts/composers.php
@@ -14,7 +14,7 @@ View::composer(array('l4layouts::bootstrap.blank'),function($view)use($secure,$a
     // Grab the existing data bound to the view
     $existingData = $view->getData();
 
-    $assets = ($existingData['assets']) ?: $assets;
+    $assets = (isset($existingData['assets'])) ? $existingData['assets'] : $assets;
     $assets->collection('header')->addCss( asset('packages/codenamegary/l4layouts/bootstrap/css/bootstrap.min.css', $secure ) );
     $assets->collection('footer')->addJs( asset('packages/codenamegary/l4layouts/bootstrap/js/bootstrap.min.js', $secure ) );
     $view->with( 'assets', $assets );
@@ -29,7 +29,7 @@ View::composer(array('l4layouts::foundation.blank'),function($view)use($secure,$
     // Grab the existing data bound to the view
     $existingData = $view->getData();
 
-    $assets = ($existingData['assets']) ?: $assets;
+    $assets = (isset($existingData['assets'])) ? $existingData['assets'] : $assets;
     // Init and create a footer collection if one doesn't already exist
     $assets->collection('footer');
     $assets->collection('header')->addCss( asset('packages/codenamegary/l4layouts/foundation/css/normalize.css', $secure ) );
